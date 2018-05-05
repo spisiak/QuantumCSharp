9c22ecabc62ccb9ffaecc7b0487988359eace8c4
Milan Spisiak <spisiak.m@outlook.com>
2018-05-05 22:06:19 +0200

    Add experimental functions
    Add experimental functions

---
+++ Experimental/Functions/QuantumExperimentalFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantumCSharp.Experimental.Functions
+{
+    public class QuantumExperimentalFunctions<T> where T : IQubit
+    {
+        public void if_else_statement(T ControlQubit, double ConditionRotation, T IfQubit, T ElseQubit)
+        {
+            IfQubit.U3((-ConditionRotation).ToString(), "0", "0");
+            IfQubit.CNOT(ControlQubit);
+            IfQubit.U3((ConditionRotation).ToString(), "0", "0");
+            ControlQubit.U3("-pi","0","0");
+            ControlQubit.CNOT(IfQubit);
+            ControlQubit.U3("pi", "0", "0");
+            ElseQubit.CNOT(ControlQubit);
+            ControlQubit.U3("-pi", "0", "0");
+            ControlQubit.CNOT(IfQubit);
+            ControlQubit.U3("pi", "0", "0");
+            ControlQubit.CNOT(IfQubit);
+        }
+
+        public void if_else_statement(T ControlQubit, T IfQubit, T ElseQubit)
+        {
+            IfQubit.CNOT(ControlQubit);
+            ElseQubit.X();
+            ElseQubit.CNOT(IfQubit);
+        }
+    }
+}
